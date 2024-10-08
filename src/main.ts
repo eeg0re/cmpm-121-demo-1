64813f8c9fcd94ed120a868ba11adceca6ed067c
Igor Bessa <ibessa@ucsc.edu>
2024-10-08 14:58:01 -0700

    added text for counting pets (Step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,26 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// add a button
+// adding a button
 const clicker = document.createElement("button");
 clicker.innerHTML = "🐈";
 app.append(clicker);
+
+
+// make a variable to store the number of clicks we get
+let counter: number = 0;
+const game_message = document.createElement("div");
+game_message.innerHTML = `${counter} pets given`;
+app.append(game_message);
+
+// function for increasing the counter
+function IncreaseCounter(this: HTMLButtonElement){
+    // increase the counting vairable
+    counter++;
+    // display the updated message
+    game_message.innerHTML = `${counter} pets given`;
+    return;
+}
+
+// check for button clicks
+clicker.addEventListener("click", IncreaseCounter);
