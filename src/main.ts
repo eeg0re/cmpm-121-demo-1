ca5fd0f38ca4f8f507d092132ab1ede671470b6f
Igor Bessa <ibessa@ucsc.edu>
2024-10-08 14:29:23 -0700

    Added working button with an emoji (Step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+// add the game name
 const gameName = "My really cool game";
 document.title = gameName;
 
+// add a header
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// add a button
+const clicker = document.createElement("button");
+clicker.innerHTML = "🐈";
+app.append(clicker);
