90fc95014495359a8b081c397785d73376887684
Igor Bessa <ibessa@ucsc.edu>
2024-10-09 13:44:59 -0700

    Added auto-petting (Step 3 Complete)

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ const clicker = document.createElement("button");
 clicker.innerHTML = "🐈";
 app.append(clicker);
 
-
 // make a variable to store the number of clicks we get
 let counter: number = 0;
 const game_message = document.createElement("div");
@@ -24,13 +23,16 @@ game_message.innerHTML = `${counter} pets given`;
 app.append(game_message);
 
 // function for increasing the counter
-function IncreaseCounter(this: HTMLButtonElement){
-    // increase the counting vairable
-    counter++;
-    // display the updated message
-    game_message.innerHTML = `${counter} pets given`;
-    return;
+function IncreaseCounter(this: HTMLButtonElement) {
+  // increase the counting vairable
+  counter++;
+  // display the updated message
+  game_message.innerHTML = `${counter} pets given`;
+  return;
 }
 
 // check for button clicks
 clicker.addEventListener("click", IncreaseCounter);
+
+// set up the auto-clicker 
+setInterval(IncreaseCounter, 1000);
