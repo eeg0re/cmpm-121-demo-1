0aaf3e6befee25e6425b32c87882e3f8605f682f
Igor Bessa <ibessa@ucsc.edu>
2024-10-09 21:43:23 -0700

    functional auto petter button (Step 5 complete)

--- src/main.ts
+++ src/main.ts
@@ -31,8 +31,8 @@ function IncreaseClickCounter(this: HTMLButtonElement) {
   return;
 }
 
-// function to display the current number of pets 
-function DisplayPets(){
+// function to display the current number of pets
+function DisplayPets() {
   const formattedCount = counter.toFixed(1);
   game_message.innerHTML = formattedCount + " times pet";
 }
@@ -41,12 +41,15 @@ clicker.addEventListener("click", IncreaseClickCounter);
 
 // set up the autoclicker that increments based on framerate
 let startTime: number;
-let prevTime: number = 0; 
-function step(timestamp: number){
-  if(startTime === undefined){
+let prevTime: number = 0;
+let auto_multiplier: number = 1; 
+function step(timestamp: number) {
+  if (startTime === undefined) {
     startTime = timestamp;
+    prevTime = startTime;
   }
 
+  
   // store the elapsed time by subtracting the current time from the last time we recorded
   const elapsed = timestamp - prevTime;
 
@@ -54,14 +57,50 @@ function step(timestamp: number){
   prevTime = timestamp;
 
   // time is in milliseconds, so we divide elapsed by 1000 to get the correct unit
-  const increment = elapsed / 1000;
-
-  // add increment to counter 
-  counter += increment; 
+  const increment = (elapsed / 1000) * auto_multiplier;
+  // add increment to counter
+  counter += increment;
 
   DisplayPets();
-
   requestAnimationFrame(step);
 }
 
-requestAnimationFrame(step);
+// add a button for players to purrchase a kitty petter
+const autoPetButton = document.createElement("button");
+autoPetButton.disabled = true;
+autoPetButton.innerHTML = "Kitty Petter 1000";
+app.append(autoPetButton);
+autoPetButton.addEventListener("click", ActivatePetter);
+autoPetButton.addEventListener("mouseover", CheckFunds);  // mouse check player funds each
+autoPetButton.addEventListener("mouseout", CheckFunds);   // time them mouse over or leave the button
+let kittyPetter: boolean = false; // use this to check if the player has bought the kitty petter
+let firstAutoPet: boolean = true; // check if it's the first time the player has bought this
+let autoCost: number = 10; 
+
+function CheckFunds(this: HTMLButtonElement){
+  if(counter >= autoCost){
+    autoPetButton.disabled = false; 
+  }
+  else{
+    autoPetButton.disabled = true; 
+  }
+}
+
+function ActivatePetter(this: HTMLButtonElement){
+  if(counter >= autoCost){
+    if(!firstAutoPet){
+      auto_multiplier *= 1.1; // increase the amount of auto petting by a little bit
+      autoCost *= 1.1;  // increase the cost little by little
+    }
+    counter -= autoCost; 
+    kittyPetter = true;
+    firstAutoPet = false;
+  }
+
+  // bool used to call the auto-clicker
+  if(kittyPetter){
+    requestAnimationFrame(step);
+  }
+}
+
+
