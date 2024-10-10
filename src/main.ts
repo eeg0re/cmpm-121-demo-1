4d8ed7e64276332f95ae32bfe37d288cb0a56ffe
Igor Bessa <ibessa@ucsc.edu>
2024-10-09 20:58:10 -0700

    finally got autoclicker working (Step 4 Complete)

--- src/main.ts
+++ src/main.ts
@@ -19,20 +19,49 @@ app.append(clicker);
 // make a variable to store the number of clicks we get
 let counter: number = 0;
 const game_message = document.createElement("div");
-game_message.innerHTML = `${counter} pets given`;
+DisplayPets();
 app.append(game_message);
 
 // function for increasing the counter
-function IncreaseCounter(this: HTMLButtonElement) {
+function IncreaseClickCounter(this: HTMLButtonElement) {
   // increase the counting vairable
   counter++;
   // display the updated message
-  game_message.innerHTML = `${counter} pets given`;
+  DisplayPets();
   return;
 }
 
+// function to display the current number of pets 
+function DisplayPets(){
+  const formattedCount = counter.toFixed(1);
+  game_message.innerHTML = formattedCount + " times pet";
+}
 // check for button clicks
-clicker.addEventListener("click", IncreaseCounter);
+clicker.addEventListener("click", IncreaseClickCounter);
 
-// set up the auto-clicker 
-setInterval(IncreaseCounter, 1000);
+// set up the autoclicker that increments based on framerate
+let startTime: number;
+let prevTime: number = 0; 
+function step(timestamp: number){
+  if(startTime === undefined){
+    startTime = timestamp;
+  }
+
+  // store the elapsed time by subtracting the current time from the last time we recorded
+  const elapsed = timestamp - prevTime;
+
+  // record this time for the next time we do math
+  prevTime = timestamp;
+
+  // time is in milliseconds, so we divide elapsed by 1000 to get the correct unit
+  const increment = elapsed / 1000;
+
+  // add increment to counter 
+  counter += increment; 
+
+  DisplayPets();
+
+  requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
