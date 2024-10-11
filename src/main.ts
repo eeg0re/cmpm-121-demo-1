d1d3cccd0c6f9a3334a80f3dac7f9dc814863730
Igor Bessa <ibessa@ucsc.edu>
2024-10-10 22:57:55 -0700

    added price increases (Step 7 Complete)

--- src/main.ts
+++ src/main.ts
@@ -19,16 +19,15 @@ const clicker = document.createElement("button");
 clicker.innerHTML = "🐈";
 app.append(clicker);
 // make it bigger
-clicker.style.width = '200px';
-clicker.style.height = '200px';
-clicker.style.fontSize = '50px';
+clicker.style.width = "200px";
+clicker.style.height = "200px";
+clicker.style.fontSize = "50px";
 
 // make a variable to store the number of clicks we get
 let counter: number = 0;
-if(DEBUG){
+if (DEBUG) {
   counter = 1000;
 }
-
 
 const game_message = document.createElement("div");
 DisplayPets();
@@ -51,7 +50,7 @@ function DisplayPets() {
 // check for button clicks
 clicker.addEventListener("click", IncreaseClickCounter);
 
-// 
+//
 let startTime: number;
 let prevTime: number = 0;
 function step(timestamp: number, buttonInfo: UpgradeButton) {
@@ -67,12 +66,13 @@ function step(timestamp: number, buttonInfo: UpgradeButton) {
   prevTime = timestamp;
 
   // time is in milliseconds, so we divide elapsed by 1000 to get the correct unit
-  const increment = (elapsed / 1000) * buttonInfo.growthRate * buttonInfo.timesBought;
+  const increment =
+    (elapsed / 1000) * buttonInfo.growthRate * buttonInfo.timesBought; // multiply the increment by this item's growth rate and the number of that item
   // add increment to counter
   counter += increment;
 
   DisplayPets();
-  requestAnimationFrame(function(timestamp: number){
+  requestAnimationFrame(function (timestamp: number) {
     step(timestamp, buttonInfo);
   });
 }
@@ -82,28 +82,34 @@ interface UpgradeButton {
   label: string;
   firstPurchase: boolean;
   cost: number;
-  growthRate: number; 
+  growthRate: number;
   active: boolean;
   timesBought: number;
 }
 
 // function that makes upgrades
-function makeUpgrade(attrs: UpgradeButton){
+function makeUpgrade(attrs: UpgradeButton) {
   const button = document.createElement("button");
   button.innerHTML = attrs.label;
-  if(!attrs.active){
+  if (!attrs.active) {
     button.disabled = true;
   }
-  button.addEventListener("click", () => {ActivateUpgrade(attrs);});
-  button.addEventListener("mouseover", () => {CheckFunds(button, attrs);}); // mouse check player funds each
-  button.addEventListener("mouseout", () => {CheckFunds(button, attrs);}); // time them mouse over or leave the button
+  button.addEventListener("click", () => {
+    ActivateUpgrade(attrs);
+  });
+  button.addEventListener("mouseover", () => {
+    CheckFunds(button, attrs);
+  }); // mouse check player funds each
+  button.addEventListener("mouseout", () => {
+    CheckFunds(button, attrs);
+  }); // time them mouse over or leave the button
 
   app.append(button);
 }
 // function checks if player has enough to buy the current button and changes button state
 function CheckFunds(button: HTMLButtonElement, buttonInfo: UpgradeButton) {
   if (counter >= buttonInfo.cost) {
-    button.disabled = false;  // actually changes button status
+    button.disabled = false; // actually changes button status
     buttonInfo.active = true; // tracks button status in the interface
   } else {
     button.disabled = true;
@@ -111,18 +117,19 @@ function CheckFunds(button: HTMLButtonElement, buttonInfo: UpgradeButton) {
   }
 }
 
-// function that will activate an upgrade if 
-function ActivateUpgrade( buttonInfo: UpgradeButton) {
+// function that will activate an upgrade if
+function ActivateUpgrade(buttonInfo: UpgradeButton) {
   if (counter >= buttonInfo.cost) {
     counter -= buttonInfo.cost;
     buttonInfo.active = true;
     buttonInfo.firstPurchase = false;
     buttonInfo.timesBought += 1;
+    buttonInfo.cost *= 1.15;    // increase the cost by a little bit
     DisplayStats();
   }
   // bool used to actually  the auto-clicker
   if (buttonInfo.active) {
-    requestAnimationFrame(function(timestamp: number){
+    requestAnimationFrame(function (timestamp: number) {
       step(timestamp, buttonInfo);
     });
   }
@@ -133,19 +140,19 @@ const upgradeList: UpgradeButton[] = [];
 // first upgrade, cost 10 pets, produces 0.1 pets/sec
 const kittyPetter1000: UpgradeButton = {
   label: "Kitty Petter 1000",
-  firstPurchase: true, 
+  firstPurchase: true,
   cost: 10,
   growthRate: 0.1,
   active: false,
   timesBought: 0,
 };
 makeUpgrade(kittyPetter1000); // initialize our 1st upgrade button
-upgradeList.push(kittyPetter1000);  // add this upgrade to our list of upgrades
+upgradeList.push(kittyPetter1000); // add this upgrade to our list of upgrades
 
 // second upgrade, cost 100 pets, produces 2 pets/sec
 const DoubleArms: UpgradeButton = {
   label: "Double Arms",
-  firstPurchase: true, 
+  firstPurchase: true,
   cost: 100,
   growthRate: 2.0,
   active: false,
@@ -157,7 +164,7 @@ upgradeList.push(DoubleArms);
 // third upgrade, cost 100 pets, produces 2 pets/sec
 const KittyCompanion: UpgradeButton = {
   label: "Kitty Companion",
-  firstPurchase: true, 
+  firstPurchase: true,
   cost: 1000,
   growthRate: 50.0,
   active: false,
@@ -171,15 +178,15 @@ const stats = document.createElement("div");
 app.append(stats);
 // stats.innerHTML;
 
-function DisplayStats(){
+function DisplayStats() {
   stats.innerHTML = "";
   let petRate = 0;
-  for (let i = 0; i < upgradeList.length; i++){
-    stats.innerHTML += upgradeList[i].label + ": " + upgradeList[i].timesBought + "\n";
-    if(upgradeList[i].active){
+  for (let i = 0; i < upgradeList.length; i++) {
+    stats.innerHTML +=
+      upgradeList[i].label + ": " + upgradeList[i].timesBought + "\n";
+    if (upgradeList[i].active) {
       petRate += upgradeList[i].growthRate * upgradeList[i].timesBought;
     }
-    
   }
   stats.innerHTML += "\nPets per second: " + petRate.toFixed(1);
 }
